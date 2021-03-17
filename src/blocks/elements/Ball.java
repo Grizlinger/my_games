ba65ffadad50ab1c52015c04e42ec4d333c5ef85
Grizlinger <szilniczky.botond@gmail.com>
2021-03-17 19:39:03 +0100

    Make Ball bounce from Stick and direction fix

--- src/blocks/elements/Ball.java
+++ src/blocks/elements/Ball.java
@@ -3,14 +3,24 @@ package blocks.elements;
 import javafx.scene.Node;
 import javafx.scene.shape.Circle;
 
+import java.util.Random;
+
 public class Ball extends Circle {
     private static final int RADIUS = 10;
-    private int vx;
-    private int vy;
+    private double speed;
+    private double vx;
+    private double vy;
 
     public Ball() {
-        vx = 5;
-        vy = 5;
+        speed = 5;
+        Random random = new Random();
+        double d = random.nextDouble() * 2 - 1;
+        vx = Math.sin(d);
+        vy = -Math.cos(d);
+        if (random.nextBoolean())
+            vx *= -1;
+        if (random.nextBoolean())
+            vy *= -1;
         setRadius(RADIUS);
         setCenterX(200);
         setCenterY(200);
@@ -21,16 +31,26 @@ public class Ball extends Circle {
         double width = this.getScene().getWidth();
         if (getCenterX() - RADIUS <= 0 || getCenterX() + RADIUS >= width)
             vx *= -1;
-        if (getCenterY() - RADIUS <= 0 || getCenterY() + RADIUS >= height)
+        if (getCenterY() - RADIUS <= 0)
             vy *= -1;
-        Stick stick;
-        for (Node p :
-                this.getParent().getChildrenUnmodifiable()) {
+        if (getCenterY() + RADIUS > height)
+            vy *= -1;
+        Stick stick = null;
+        for (Node p : this.getParent().getChildrenUnmodifiable()) {
             if (p.getClass() == Stick.class)
                 stick = (Stick) p;
         }
-        if (getCenterX() - RADIUS <= 0 || getCenterX() + RADIUS >= stick.getX() + stick.getWidth())
-            setCenterX(getCenterX() + vx);
-        setCenterY(getCenterY() + vy);
+        if (stick != null
+                && getCenterY() + RADIUS > stick.getY()
+                && getCenterY() + RADIUS < stick.getY() + Math.abs(speed)
+                && getCenterX() >= stick.getX()
+                && getCenterX() <= stick.getX() + stick.getWidth()) {
+            double hitX = (getCenterX() - stick.getX() - stick.getWidth() / 2) / (stick.getWidth() / 2);
+            vx = Math.sin(hitX);
+            vy = -Math.cos(hitX);
+        }
+
+        setCenterX(getCenterX() + speed * vx);
+        setCenterY(getCenterY() + speed * vy);
     }
 }
